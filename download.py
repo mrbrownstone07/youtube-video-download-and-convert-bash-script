9825826f90dfe8ff001170982606a2b10343ee80
mrbrownstone07 <mahdi.mad07@gmail.com>
2019-10-10 00:34:54 +0600

    cross matching links, no song from same link will be downloaded now

--- download.py
+++ download.py
@@ -1,11 +1,18 @@
 def readAndDownload():
     import os
-    f = open("links.txt", "r")
+    f = open("download_links.txt", "r")
     for x in f:
-        cmd = "mp3.sh "+x
+        cmd = "~/mp3/mp3.sh "+x
         os.system(cmd)
     
 def clearFile():
+    fin = open('download_links.txt', "r")
+    data = fin.read()
+    fin.close()
+    fout = open('download_history.txt', "a")
+    fout.write(data)
+    fout.close()            
+    open('download_links.txt', 'w').close()
     open('links.txt', 'w').close()
 readAndDownload()
 clearFile()
---
+++ getMusic.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd ~/mp3
+vim ~/mp3/links.txt
+~/mp3/./match
+python3 ~/mp3/download.py 
